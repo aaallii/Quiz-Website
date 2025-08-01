e12b3f460d9c05c0da55549189620198e3abd922
aaallii <Aliburakplaying@gmail.com>
2025-08-01 17:54:02 +0300

    Update webmaster.js
    updated with bug fixes

--- docs/webmaster.js
+++ docs/webmaster.js
@@ -1,15 +1,18 @@
-const template='<form>
-    <label>${jvalue1}</label>
-    <br>
-    <img src='${jvalue2}'>
-    <br>
-    <input type="checkbox' id='${"c"+'jvalue'}' value="1">
-    <label for='${"c"+'jvalue'}'>${jvalue3}</label><br>
-    <input type="checkbox" id='${"c"+'jvalue+1'}' value="3">
-    <label for='${"c"+'jvalue+1'}'>${jvalue4}</label><br>
-    <input type="checkbox" id='${"c"+'jvalue+2'}' value="-1">
-    <label for='${"c"+'jvalue+2'}'>${jvalue5}</label>
-</form>'
+const template = (jvalue, jvalue1, jvalue2, jvalue3, jvalue4, jvalue5) => `
+  <form>
+    <label>${jvalue1}</label><br>
+    <img src="${jvalue2}"><br>
+
+    <input type="checkbox" id="c${jvalue}" value="1">
+    <label for="c${jvalue}">${jvalue3}</label><br>
+
+    <input type="checkbox" id="c${jvalue + 1}" value="3">
+    <label for="c${jvalue + 1}">${jvalue4}</label><br>
+
+    <input type="checkbox" id="c${jvalue + 2}" value="-1">
+    <label for="c${jvalue + 2}">${jvalue5}</label>
+  </form>
+`;
 fetch("imgdata.json")
   .then(response => response.json())
   .then(data => {
