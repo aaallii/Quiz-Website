45b0d211dcfae193f65e9dcdac547a48fc63da26
aaallii <Aliburakplaying@gmail.com>
2025-08-01 17:49:21 +0300

    Update webmaster.js
    template has been updated to variables

--- docs/webmaster.js
+++ docs/webmaster.js
@@ -1,10 +1,15 @@
-const template="    <input type="checkbox" id="c1" value="1">
-    <label for="c1">jimbo</label><br>
-    <input type="checkbox" id="c2" value="3">
-    <label for="c2">Jiiimbo</label><br>
-    <input type="checkbox" id="c3" value="-1">
-    <label for="c3">jmbo</label>
-</form>"
+const template='<form>
+    <label>${jvalue1}</label>
+    <br>
+    <img src='${jvalue2}'>
+    <br>
+    <input type="checkbox' id='${"c"+'jvalue'}' value="1">
+    <label for='${"c"+'jvalue'}'>${jvalue3}</label><br>
+    <input type="checkbox" id='${"c"+'jvalue+1'}' value="3">
+    <label for='${"c"+'jvalue+1'}'>${jvalue4}</label><br>
+    <input type="checkbox" id='${"c"+'jvalue+2'}' value="-1">
+    <label for='${"c"+'jvalue+2'}'>${jvalue5}</label>
+</form>'
 fetch("imgdata.json")
   .then(response => response.json())
   .then(data => {
