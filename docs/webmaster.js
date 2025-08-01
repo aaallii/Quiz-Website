b9a4dd7ffad9423ee269959f9bca19dbc56b3954
aaallii <Aliburakplaying@gmail.com>
2025-08-01 17:12:15 +0300

    Update webmaster.js
    first half of const template added

--- docs/webmaster.js
+++ docs/webmaster.js
@@ -1,3 +1,10 @@
+const template="    <input type="checkbox" id="c1" value="1">
+    <label for="c1">jimbo</label><br>
+    <input type="checkbox" id="c2" value="3">
+    <label for="c2">Jiiimbo</label><br>
+    <input type="checkbox" id="c3" value="-1">
+    <label for="c3">jmbo</label>
+</form>"
 fetch("imgdata.json")
   .then(response => response.json())
   .then(data => {
